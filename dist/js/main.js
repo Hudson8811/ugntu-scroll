e81e6d3220ca253ae18ad98cc84bcb2cd6ec62f1
PipZip <37911683+PipZip@users.noreply.github.com>
2023-02-07 21:59:02 +0600

    temp

--- dist/js/main.js
+++ dist/js/main.js
@@ -833,6 +833,182 @@ __webpack_require__.r(__webpack_exports__);
 
 gsap__WEBPACK_IMPORTED_MODULE_1__["default"].registerPlugin(gsap_ScrollTrigger__WEBPACK_IMPORTED_MODULE_2__["default"]);
 gsap__WEBPACK_IMPORTED_MODULE_1__["default"].registerPlugin(gsap_MotionPathPlugin__WEBPACK_IMPORTED_MODULE_3__["default"]);
+var initMode;
+var addTime = 800;
+var st1, st2, st3;
+var tl1 = gsap__WEBPACK_IMPORTED_MODULE_1__["default"].timeline({});
+var tl2 = gsap__WEBPACK_IMPORTED_MODULE_1__["default"].timeline({});
+var tl3 = gsap__WEBPACK_IMPORTED_MODULE_1__["default"].timeline({});
+
+function initAnimation() {
+  if (window.innerWidth > 1100) {
+    initScrollAnimationDesktop();
+    initMode = 'desk';
+  } else if (window.innerWidth > 699) {
+    initScrollAnimationTablet();
+    initMode = 'tablet';
+  } else {
+    initScrollAnimationMobile();
+    initMode = 'mobile';
+  }
+}
+
+initAnimation();
+var dwidth = jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width();
+jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).on('resize', function () {
+  var wwidth = jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width();
+
+  if (dwidth !== wwidth) {
+    if (typeof st1 !== "undefined") st1.kill();
+    if (typeof st2 !== "undefined") st2.kill();
+    if (typeof st3 !== "undefined") st3.kill();
+    tl.clear();
+    tl1.clear();
+    tl2.clear();
+    tl3.clear();
+    gsap__WEBPACK_IMPORTED_MODULE_1__["default"].set("" + ".scroll-page", {
+      clearProps: "all"
+    });
+    window.scrollTo({
+      top: 0,
+      behavior: "instant"
+    });
+    jquery__WEBPACK_IMPORTED_MODULE_0___default()('body').removeClass('active');
+    initAnimation();
+  }
+});
+
+function initScrollAnimationDesktop() {
+  jquery__WEBPACK_IMPORTED_MODULE_0___default()('body').addClass('active');
+  tl1.fromTo(".y2-1__numbers", {
+    x: '100vw'
+  }, {
+    x: '-100%',
+    duration: 6,
+    ease: "none"
+  }, "0");
+  tl1.fromTo(".y2-1__photos-item--1", {
+    x: '100vw'
+  }, {
+    x: '-100%',
+    duration: 1.5,
+    ease: "none"
+  }, "<+1");
+  tl1.fromTo(".y2-1__photos-item--2", {
+    x: '100vw'
+  }, {
+    x: '-100%',
+    duration: 1.5,
+    ease: "none"
+  }, ">-0.2");
+  tl1.fromTo(".y2-1__photos-item--3", {
+    x: '100vw'
+  }, {
+    x: '-100%',
+    duration: 1.5,
+    ease: "none"
+  }, ">-0.7");
+  tl1.fromTo(".y2-2", {
+    autoAlpha: 0
+  }, {
+    autoAlpha: 1,
+    duration: 0.7,
+    ease: "none"
+  }, "6");
+  tl1.fromTo(".y2-2__top", {
+    y: 0
+  }, {
+    y: "-100%",
+    duration: 2,
+    ease: "none"
+  }, ">0.3");
+  tl1.fromTo(".y2-2__title", {
+    y: "100vh"
+  }, {
+    y: "0",
+    duration: 2,
+    ease: "none"
+  }, "<0.2");
+  tl1.fromTo(".y2-2__fly", {
+    y: 0,
+    top: "100%"
+  }, {
+    y: "-100%",
+    top: 0,
+    duration: 1,
+    ease: "none"
+  }, ">-0.4");
+  tl1.from(".y2-2__title span:nth-child(1)", {
+    x: "130%",
+    duration: 3,
+    ease: "none"
+  }, "<-1.6");
+  tl1.from(".y2-2__title span:nth-child(2)", {
+    x: "-80%",
+    duration: 3,
+    ease: "none"
+  }, "<");
+  tl1.from(".y2-2__title span:nth-child(3)", {
+    x: "50%",
+    duration: 3,
+    ease: "none"
+  }, "<");
+  tl1.fromTo(".y2-2__text", {
+    autoAlpha: 0
+  }, {
+    autoAlpha: 1,
+    duration: 0.3,
+    ease: "none"
+  }, ">");
+  tl1.fromTo(".y2-2__title", {
+    autoAlpha: 1
+  }, {
+    autoAlpha: 0.2,
+    duration: 0.2,
+    ease: "none"
+  }, ">");
+  tl1.fromTo(".y2-2__diplom", {
+    y: 0,
+    top: "100%"
+  }, {
+    y: "-100%",
+    top: 0,
+    duration: 2,
+    ease: "none"
+  }, "<");
+  tl1.fromTo(".y2-1", {
+    top: "0",
+    autoAlpha: 1
+  }, {
+    top: "-100%",
+    autoAlpha: 0,
+    duration: 0,
+    ease: "none"
+  }, ">");
+  tl1.fromTo(".y2-2", {
+    y: 0,
+    top: "0"
+  }, {
+    y: "0",
+    top: "-100%",
+    duration: 2,
+    ease: "none"
+  }, "<");
+  st1 = gsap_ScrollTrigger__WEBPACK_IMPORTED_MODULE_2__["default"].create({
+    trigger: ".scroll-page",
+    pin: true,
+    start: "top top",
+    end: function end() {
+      return "+=" + addTime + "%";
+    },
+    scrub: 2.5,
+    animation: tl1
+  });
+}
+
+function initScrollAnimationTablet() {}
+
+function initScrollAnimationMobile() {}
 
 /***/ }),
 
--- src/js/import/years/_y1950.js
+++ src/js/import/years/_y1950.js
@@ -4,3 +4,205 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import MotionPathPlugin from "gsap/MotionPathPlugin";
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
+
+
+var initMode;
+let addTime = 800;
+
+
+let st1, st2, st3;
+let tl1 = gsap.timeline({});
+let tl2 = gsap.timeline({});
+let tl3 = gsap.timeline({});
+
+
+
+function initAnimation(){
+    if (window.innerWidth > 1100){
+        initScrollAnimationDesktop();
+        initMode = 'desk';
+    } else if(window.innerWidth > 699){
+        initScrollAnimationTablet();
+        initMode = 'tablet';
+    } else {
+        initScrollAnimationMobile();
+        initMode = 'mobile';
+    }
+}
+
+initAnimation();
+
+var dwidth = $(window).width();
+$(window).on('resize',function (){
+    var wwidth = $(window).width();
+    if(dwidth!==wwidth){
+        if (typeof st1 !== "undefined") st1.kill();
+        if (typeof st2 !== "undefined") st2.kill();
+        if (typeof st3 !== "undefined") st3.kill();
+        tl.clear();
+        tl1.clear();
+        tl2.clear();
+        tl3.clear();
+        gsap.set("" +
+            ".scroll-page", {clearProps:"all"});
+        window.scrollTo({
+            top: 0,
+            behavior: "instant"
+        });
+        $('body').removeClass('active');
+        initAnimation();
+    }
+});
+
+
+function initScrollAnimationDesktop(){
+    $('body').addClass('active');
+
+    tl1.fromTo(".y2-1__numbers", {
+        x: '100vw',
+    }, {
+        x: '-100%',
+        duration:  6,
+        ease: "none",
+    } , "0");
+    tl1.fromTo(".y2-1__photos-item--1", {
+        x: '100vw',
+    }, {
+        x: '-100%',
+        duration:  1.5,
+        ease: "none",
+    } , "<+1");
+    tl1.fromTo(".y2-1__photos-item--2", {
+        x: '100vw',
+    }, {
+        x: '-100%',
+        duration:  1.5,
+        ease: "none",
+    } , ">-0.2");
+    tl1.fromTo(".y2-1__photos-item--3", {
+        x: '100vw',
+    }, {
+        x: '-100%',
+        duration:  1.5,
+        ease: "none",
+    } , ">-0.7");
+
+
+    tl1.fromTo(".y2-2", {
+        autoAlpha: 0,
+    }, {
+        autoAlpha: 1,
+        duration: 0.7,
+        ease: "none",
+    } , "6");
+
+
+    tl1.fromTo(".y2-2__top", {
+        y: 0,
+    }, {
+        y: "-100%",
+        duration: 2,
+        ease: "none",
+    } , ">0.3");
+
+    tl1.fromTo(".y2-2__title", {
+        y: "100vh"
+    }, {
+        y: "0",
+        duration: 2,
+        ease: "none",
+    } , "<0.2");
+
+
+    tl1.fromTo(".y2-2__fly", {
+        y: 0,
+        top: "100%"
+    }, {
+        y: "-100%",
+        top: 0,
+        duration: 1,
+        ease: "none",
+    } , ">-0.4");
+
+    tl1.from(".y2-2__title span:nth-child(1)", {
+        x: "130%",
+        duration: 3,
+        ease: "none",
+    } , "<-1.6");
+
+    tl1.from(".y2-2__title span:nth-child(2)", {
+        x: "-80%",
+        duration: 3,
+        ease: "none",
+    } , "<");
+
+    tl1.from(".y2-2__title span:nth-child(3)", {
+        x: "50%",
+        duration: 3,
+        ease: "none",
+    } , "<");
+
+
+
+    tl1.fromTo(".y2-2__text", {
+        autoAlpha: 0,
+    }, {
+        autoAlpha: 1,
+        duration: 0.3,
+        ease: "none",
+    } , ">");
+
+    tl1.fromTo(".y2-2__title", {
+        autoAlpha: 1,
+    }, {
+        autoAlpha: 0.2,
+        duration: 0.2,
+        ease: "none",
+    } , ">");
+
+
+    tl1.fromTo(".y2-2__diplom", {
+        y: 0,
+        top: "100%"
+    }, {
+        y: "-100%",
+        top: 0,
+        duration: 2,
+        ease: "none",
+    } , "<");
+
+
+    tl1.fromTo(".y2-1", {
+        top: "0",
+        autoAlpha: 1,
+    }, {
+        top: "-100%",
+        autoAlpha: 0,
+        duration: 0,
+        ease: "none",
+    } , ">");
+    tl1.fromTo(".y2-2", {
+        y: 0,
+        top: "0"
+    }, {
+        y: "0",
+        top: "-100%",
+        duration: 2,
+        ease: "none",
+    } , "<");
+
+
+
+
+    st1 = ScrollTrigger.create({
+        trigger: ".scroll-page",
+        pin: true,
+        start: "top top",
+        end: () =>   "+="+addTime+"%",
+        scrub: 2.5,
+        animation: tl1,
+    });
+}
+
+function initScrollAnimationTablet(){}
+function initScrollAnimationMobile(){}
